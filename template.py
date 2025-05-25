c221762a01b05afc06a72cc4095415c736b8c1af
Nabarun Dutta <nabarundutta@Nabaruns-MacBook-Air.local>
2025-05-25 23:41:46 +0530

    2nd commit

--- template.py
+++ template.py
@@ -1,7 +1,7 @@
 import os
 import logging
 from pathlib import Path
-logging.basicConfig(level=logging.INFO,format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',filename='app.log')
+logging.basicConfig(level=logging.INFO,format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 project_name = "datascience"
 
 list_of_items = [
