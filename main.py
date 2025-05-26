f12af3d40349af6395f39f9b9da96e3dfc91159e
Nabarun Dutta <nabarundutta@Nabaruns-MacBook-Air.local>
2025-05-26 16:18:45 +0530

    Data ingesion pipeline created

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from src.datascience import logger
+from src.datascience.pipeline.data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("hello welcome to this project")
+
+
+STAGE_NAME = "DATA INGESTION STEP"
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.initiate_data_ingestion()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
